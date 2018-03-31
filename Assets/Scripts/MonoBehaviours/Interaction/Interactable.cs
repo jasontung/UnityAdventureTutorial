f6919523a66bbf54c7be2509c15d45d9dd6fe9cf
jasontung <jasontung@dongweichengde-MacBook-Pro.local>
2018-03-31 11:12:18 +0800

    1. redesign from start state

--- Assets/Scripts/MonoBehaviours/Interaction/Interactable.cs
+++ Assets/Scripts/MonoBehaviours/Interaction/Interactable.cs
@@ -18,11 +18,6 @@ public class Interactable : MonoBehaviour
     // This is called when the player arrives at the interactionLocation.
     public void Interact ()
     {
-        for (int i = 0; i < conditionCollections.Length; i++)
-        {
-            if (conditionCollections[i].CheckAndReact())
-                return;
-        }
-        defaultReactionCollection.React();
+        
     }
 }
